d6da781cbb24cbf0725a5bbe61dc6c77c09f5c33
Aram <arambughdaryan@gmail.com>
2025-01-25 22:45:47 +0400

    [Improve] add some comments

--- day_1/increment.cu
+++ day_1/increment.cu
@@ -1,39 +1,44 @@
 #include <cuda_runtime.h>
 #include <iostream>
 
-__global__ void incrementKernel(int *data, int size){
+// Kernel function to increment array elements
+__global__ void incrementKernel(int *data, int size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
-        data[idx] += 2;
+        data[idx] += 1;
     }
 }
 
-int main(){
-    const long int size = 5000000;
+int main() {
+    // Host is in our case CPU and device is GPU
+    const int size = 2048;
     int *hostArray = new int[size];
     int *deviceArray;
 
+    // Initialize host array
     for (int i = 0; i < size; i++) hostArray[i] = i;
 
-    for (int i = 0; i < 30; i++) std::cout << hostArray[i] << '\t';
-
-    std::cout << '\n';
-
+    // Allocate memory on the GPU
     cudaMalloc((void **)&deviceArray, size * sizeof(int));
 
+    // Copy data to the GPU
     cudaMemcpy(deviceArray, hostArray, size * sizeof(int), cudaMemcpyHostToDevice);
 
+    // Launch kernel
     int threadsPerBlock = 256;
-    int blocksPerGrid = (size * threadsPerBlock - 1) / threadsPerBlock;
-
+    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
     incrementKernel<<<blocksPerGrid, threadsPerBlock>>>(deviceArray, size);
 
+    // Copy result back to the host
     cudaMemcpy(hostArray, deviceArray, size * sizeof(int), cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < 30; i++) std::cout << hostArray[i] << '\t';
-    
+    // Print result
+    for (int i = 0; i < 10; i++) std::cout << hostArray[i] << " ";
+    std::cout << std::endl;
+
+    // Clean up
     cudaFree(deviceArray);
-    delete [] hostArray;
+    delete[] hostArray;
 
     return 0;
 }
