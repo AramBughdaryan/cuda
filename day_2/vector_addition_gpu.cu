45a568e123d9583fde471d3435e6c8e968421473
Aram <arambughdaryan@gmail.com>
2025-01-28 16:08:07 +0400

    [Update] finalize vector addition on gpu

--- day_2/vector_addition_gpu.cu
+++ day_2/vector_addition_gpu.cu
@@ -2,12 +2,11 @@
 #include <cuda.h>
 
 __global__ void vecAddKernel(float* A, float* B, float* C, int n){
-    int i = blockDim.x * blockIdx.x + threadIdx.x
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
     if (i < n) C[i] = A[i] + B[i];
 }
 
-
-void vecAdd(float* h_A, float* h_B, float* h_C, int n)
+__host__ void vecAdd(float* h_A, float* h_B, float* h_C, int n)
 {
     int size = n * sizeof(float);
     float *d_A, *d_B, *d_C;
@@ -23,30 +22,27 @@ void vecAdd(float* h_A, float* h_B, float* h_C, int n)
     cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
     cudaMalloc((void**)&d_C, size);
+    // Here ceil(size / 256.0) is number of blocks
+    // 256 as second argument is the number of threads in each block
+    vecAddKernel<<<ceil(size / 256.0), 256>>>(d_A, d_B, d_C, n);
 
     cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-
-
+    cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
 }
 
 int main(){
     const int n = 1024;
-    float *h_A;
-    float *h_B;
-    float *h_C;
+    int i;
+    float *h_A = new float[n];
+    float *h_B = new float[n];
+    float *h_C = new float[n];
 
-    float *d_A;
-    float *d_B;
-    float *d_C;
-    int size = n * sizeof(float);
-
-    
-
-    
-
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+    for (i = 0; i < n; i++){
+        h_A[i] = i + 4;
+        h_B[i] = pow(i, 3);
+    }
+    vecAdd(h_A, h_B, h_C, n);
+    for (i = 0; i < 10; i++) std::cout<<h_C[i]<<' ';
 
 }
