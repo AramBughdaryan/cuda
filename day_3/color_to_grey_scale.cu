448773e61f92aa8f2dbab28c12e6e9479faa9b5a
Aram <arambughdaryan@gmail.com>
2025-01-29 08:46:35 +0400

    [BugFix] getting green and blue bytes with +1 and +2

--- day_3/color_to_grey_scale.cu
+++ day_3/color_to_grey_scale.cu
@@ -12,8 +12,8 @@ __global__ void colorToGreyscaleConversion(unsigned char* Pout, unsigned char* P
         int greyOffset = Row * width + Col;
         int rgbOffset = greyOffset * CHANNELS;
         unsigned char r = Pin[rgbOffset]; // red value pixel
-        unsigned char g = Pin[rgbOffset + 2]; //TODO why we add +2 here instead of +1?!
-        unsigned char b = Pin[rgbOffset + 3];
+        unsigned char g = Pin[rgbOffset + 1]; //TODO why we add +2 here instead of +1?!
+        unsigned char b = Pin[rgbOffset + 2];
 
         Pout[greyOffset] = 0.21 * r + 0.71 * g + 0.07*b;
     }
