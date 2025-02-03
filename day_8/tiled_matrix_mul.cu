6b3681e3808b7b932aa0c0ebce25e0ff37d51cc8
Aram <arambughdaryan@gmail.com>
2025-02-03 23:49:56 +0400

    [Update] implement tiled matrix mul kernel!

--- day_8/tiled_matrix_mul.cu
+++ day_8/tiled_matrix_mul.cu
@@ -14,13 +14,21 @@ __global__ void tiledMatrixMulKernel(float* d_M, float* d_N, float* d_P, int Wid
     int tx = threadIdx.x;
     int ty = threadIdx.y;
 
+    // Saved as automatic variables thus in registers. 
     int Row = by * TILE_WIDTH + ty;
     int Col = bx * TILE_WIDTH + tx;
 
     float Pvalue = 0;
-    for (int ph = 0; ph < Width / TILE_WIDTH; ++ph){
-        Mds[ty][tx] = d_M[Row * Width + ph * TILE_WIDTH + tx];
-        Nds[ty][tx] = d_N[ph * TILE_WIDTH + ph * Width + Col];
-    }
+    for (int tile = 0; tile < Width / TILE_WIDTH; ++tile){
+        // Collaborative loading of d_M and d_N tiles into shared memory
+        Mds[ty][tx] = d_M[Row * Width + tile * TILE_WIDTH + tx];
+        Nds[ty][tx] = d_N[(tile * TILE_WIDTH + threadIdx.y) * Width + Col]; 
+        __syncthreads();
 
+        for (int k =0; k < TILE_WIDTH; ++k){
+            Pvalue += Mds[ty][k] * Nds[k][tx];
+        }
+        __syncthreads();
+    }
+    d_P[Row * Width + Col] = Pvalue;
 }
