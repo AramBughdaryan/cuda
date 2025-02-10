a87e1bfa46ed7163c86a4548e059c19d8444acf0
Aram <arambughdaryan@gmail.com>
2025-02-10 23:10:39 +0400

    [Add] tests to compare Triton vs PyTorch

--- day_13/vector_addition.py
+++ day_13/vector_addition.py
@@ -3,7 +3,7 @@ import torch
 import triton
 import triton.language as tl
 
-DEVICE = triton.runtime.driver.active.get_active_torch_device()
+DEVICE = torch.device('cuda:0')
 
 @triton.jit
 def add_kernel(x_ptr, # Pointer to first input vector
@@ -25,4 +25,29 @@ def add_kernel(x_ptr, # Pointer to first input vector
 
     output = x + y
     tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x, device=DEVICE)
+    assert x.device == DEVICE and y.device == DEVICE and output.device == DEVICE
+
+    n_elements = output.numel()
+
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+
+    return output
+
+if __name__ == '__main__':
+    torch.manual_seed(42)
+    size = 98432
+    x = torch.randn(size=(size, ), device=DEVICE)
+    y = torch.randn(size=(size, ), device=DEVICE)
+    out_torch = x + y
+    out_triton = add(x, y)
+    print(out_torch, out_triton)
+
+    print(f"The maxmimum difference between torch and triton is ", 
+        f'{torch.max(torch.abs(out_torch - out_triton))}')
 
