ddd73ae1c33e07b77da237f0c6521e7fa7ef2971
Aram <arambughdaryan@gmail.com>
2025-02-10 22:45:18 +0400

    [Add[ vector addition kernel with Triton

---
+++ day_13/vector_addition.py
@@ -0,0 +1,28 @@
+import torch
+
+import triton
+import triton.language as tl
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+@triton.jit
+def add_kernel(x_ptr, # Pointer to first input vector
+                y_ptr, # Pointer to second input vector
+                output_ptr, # Pointer to output vector
+                n_elements, # size of the vector
+                BLOCK_SIZE: tl.constexpr, # Number of elements each program should process
+                ):
+    
+    pid = tl.program_id(axis=0)
+
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+
+    mask = offsets < n_elements
+
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+
