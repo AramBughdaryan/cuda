3689a547758bc68fc2e4423d7e27d4d2dd87532d
Aram <arambughdaryan@gmail.com>
2025-02-22 21:09:03 +0400

    [Add] comments for 1d conv with halo cells

--- day_21/tiled_conv_1d.cu
+++ day_21/tiled_conv_1d.cu
@@ -2,26 +2,32 @@
 #include <cuda.h>
 
 
-__global__ void convolution_1D_tiled_kernel(float *N, float *P, int Mask_Width,
-    int Width) {
+__global__ void convolution_1D_tiled_kernel(float *N, float *P, int Mask_Width, int Width) {
     int i = blockIdx.x*blockDim.x + threadIdx.x;
-    __shared__ float N_ds[TILE_SIZE + MAX_MASK_WIDTH - 1];
-    int n = Mask_Width/2;
-    int halo_index_left = (blockIdx.x - 1)*blockDim.x + threadIdx.x;
+
+    __shared__ float N_ds[TILE_SIZE + MAX_MASK_WIDTH - 1]; // N_ds[8]
+
+    int n = Mask_Width/2; // 2
+
+    int halo_index_left = (blockIdx.x - 1)*blockDim.x + threadIdx.x; 
+
     if (threadIdx.x >= blockDim.x - n) {
-    N_ds[threadIdx.x - (blockDim.x - n)] =
-    (halo_index_left < 0) ? 0 : N[halo_index_left];
-    }
-    N_ds[n + threadIdx.x] = N[blockIdx.x*blockDim.x + threadIdx.x];
+        N_ds[threadIdx.x - (blockDim.x - n)] = (halo_index_left < 0) ? 0 : N[halo_index_left];
+    } // fills 0, 1
+
+    N_ds[n + threadIdx.x] = N[blockIdx.x*blockDim.x + threadIdx.x]; // fills 2, 3, 4, 5
+
     int halo_index_right = (blockIdx.x + 1)*blockDim.x + threadIdx.x;
+
     if (threadIdx.x < n) {
-    N_ds[n + blockDim.x + threadIdx.x] =
-    (halo_index_right >= Width) ? 0 : N[halo_index_right];
-    }
+        N_ds[n + blockDim.x + threadIdx.x] = (halo_index_right >= Width) ? 0 : N[halo_index_right];
+    } // fills 7, 8
+
     __syncthreads();
+
     float Pvalue = 0;
     for(int j = 0; j < Mask_Width; j++) {
-    Pvalue += N_ds[threadIdx.x + j]*M[j];
+        Pvalue += N_ds[threadIdx.x + j]*M[j];
     }
     P[i] = Pvalue;
 }
